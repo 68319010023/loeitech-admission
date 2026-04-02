ca27c4d702f9bf08c919169fc9d9b80c1fb6ea7a
68319010023 <68319010023@loeitech.ac.th>
2026-04-02 12:47:38 +0700

    1

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,13 +5,12 @@ const router = createRouter({
   routes: [
     { path: '/', component: () => import('../views/HomeView.vue') },
     { path: '/register', component: () => import('../views/RegisterView.vue') },
-<<<<<<< HEAD
+    // --- รวม Route ที่ต้องการเข้าด้วยกัน และลบเครื่องหมาย <<<<< ===== >>>>> ออกให้หมด ---
     { path: '/status', component: () => import('../views/StatusView.vue') },
     { path: '/success', component: () => import('../views/SuccessView.vue') },
-=======
     { path: '/enrollment', component: () => import('../views/StatusView.vue') },
     { path: '/check-status', component: () => import('../views/CheckStatusView.vue') },
->>>>>>> 038a70c8f15d2b577941898b3d3cf114ac603b76
+    // --------------------------------------------------------------------------
     { path: '/admin/users', component: () => import('../views/AdminUsersView.vue') },
     { path: '/admin/settings', component: () => import('../views/AdminSettingsView.vue') },
   ],
